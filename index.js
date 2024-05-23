c2a60f363d881b0d6b86af47417e6ef5312c1db7
syeda shakera <unisyeds808@gmail.com>
2024-05-23 09:59:09 -0600

    dogs images added

--- index.js
+++ index.js
@@ -36,3 +36,36 @@ fetchCatButton.addEventListener("click", () => {
       console.warn(err);
     });
 });
+// Fetch a dog
+fetchDogBtn.addEventListener("click", () => {
+  fetch(
+    "https://api.thedogapi.com/v1/images/search?limit=1&has_breeds=1&api_key=live_cJYKf789eUk5mDuCfoXp2O9FovgZd1gbIDkftrZVjGJgtRclLAkuiP9k0OnrpmMy"
+  )
+    .then((res) => {
+      if (!res.ok) {
+        throw new Error("Invalid Request");
+      }
+      return res.json();
+    })
+    .then((data) => {
+      // Clear out previous dog
+      catOrDogContainer.innerHTML = "";
+
+      const dogImgUrl = data[0].url;
+      const dogImg = document.createElement("img");
+      dogImg.src = dogImgUrl;
+      const breedName = data[0].breeds[0].name;
+      const dogBreedTitle = document.createElement("h2");
+      dogBreedTitle.innerText = breedName;
+
+      const breedTemperament = document.createElement("p");
+      breedTemperament.innerText = data[0].breeds[0].temperament;
+
+      catOrDogContainer.appendChild(dogBreedTitle);
+      catOrDogContainer.appendChild(breedTemperament);
+      catOrDogContainer.appendChild(dogImg);
+    })
+    .catch((err) => {
+      console.warn(err);
+    });
+});
